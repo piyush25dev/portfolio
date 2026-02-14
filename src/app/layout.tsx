9ed40586ca0b6fca5ac9b76a15220622a9dbb500
piyushevdt <piyush.evdt@gmail.com>
2026-02-14 17:31:58 +0530

    [fix] "minor bug"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        // style={{ position: "relative" }}
+        style={{ position: "relative" }}
       >
         <ParticleBackground />
         <ThemeProvider theme={theme}>
